7030abae2c5c491ff8516c0a645959d5feb945de
faiznfl <faiznfl20@gmail.com>
2025-06-11 12:23:00 +0700

    setoran

---
+++ app/Filament/Resources/SetoranResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SetoranResource\Pages;
+use App\Filament\Resources\SetoranResource\RelationManagers;
+use App\Models\Setoran;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SetoranResource extends Resource
+{
+    protected static ?string $model = Setoran::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('nama')->placeholder('Masukkan Nama')->label('Nama Driver')->required(),
+                        Forms\Components\TextInput::make('mobil')->placeholder('Masukkan Nama Mobil')->label('Nama Mobil')->required(),
+                        Forms\Components\DatePicker::make('tgl_booking')->label('Tanggal Setoran')->required(),
+                        Forms\Components\TextInput::make('harga')->placeholder('Masukkan Jumlah Setoran')->label('Jumlah Setoran')->required(),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama')->label('Nama Driver')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('mobil')->label('Nama Mobil')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('tgl_booking')->label('Tanggal Setoran')->searchable(),
+                Tables\Columns\TextColumn::make('harga')->label('Jumlah Setoran')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSetorans::route('/'),
+            'create' => Pages\CreateSetoran::route('/create'),
+            'edit' => Pages\EditSetoran::route('/{record}/edit'),
+        ];
+    }
+}
