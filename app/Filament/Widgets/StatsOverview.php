d75731550629315afc7868a4d9c3a2f921e2f65b
alfathir rizky harsya <alfathirrizky03@gmail.com>
2025-06-21 21:07:57 +0700

    widget lagi

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -4,6 +4,7 @@ namespace App\Filament\Widgets;
 
 use App\Models\Review;
 use App\Models\Booking;
+use App\Models\cicilan;
 use App\Models\Setoran;
 use App\Models\UnexpectedCost;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -21,10 +22,16 @@ class StatsOverview extends BaseWidget
                 ->description('Total semua booking')
                 ->color('primary'),
             Stat::make('Total Pemasukan', 'Rp ' . number_format(Setoran::sum('harga'), 0, ',', '.'))
-                ->description('Dari semua setoran')
+                ->description('Dari semua Setoran')
                 ->color('info'),
-            Stat::make('Total Pengeluaran', 'Rp ' . number_format(UnexpectedCost::sum('harga'), 0, ',', '.'))
-                ->description('Dari semua setoran')
+            Stat::make('Total Pemasukan', 'Rp ' . number_format(Booking::sum('harga'), 0, ',', '.'))
+                ->description('Dari semua Booking')
+                ->color('info'),
+            Stat::make('Total Pengeluaran Darurat', 'Rp ' . number_format(UnexpectedCost::sum('harga'), 0, ',', '.'))
+                ->description('Dari dana darurat')
+                ->color('info'),
+            Stat::make('Total Cicilan', 'Rp ' . number_format(cicilan::sum('nominal'), 0, ',', '.'))
+                ->description('Dari semua cicilan')
                 ->color('info'),
         ];
     }
